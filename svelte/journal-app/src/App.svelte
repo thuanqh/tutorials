b711f3a7ee38c03be29a0bee58008d64a66b155d
Jason Quach <thuanqh@gmail.com>
2021-01-31 18:52:44 +0700

    Complete Svelte Journaling App

--- svelte/journal-app/src/App.svelte
+++ svelte/journal-app/src/App.svelte
@@ -1,5 +1,19 @@
 <script>
+  import Navbar from "./components/Navbar.svelte";
   import ViewAdd from "./components/ViewAdd.svelte";
+  import ViewObject from "./components/ViewObject.svelte";
+  import ViewList from "./components/ViewList.svelte";
+  import { view } from "./stores";
 </script>
 
-<ViewAdd />
+<Navbar />
+
+<div class="container mx-auto w-full lg:w-3/5 px-2 pt-2 mt-2">
+  {#if $view == "add"}
+    <ViewAdd />
+  {:else if $view && $view.startsWith("view/")}
+    <ViewObject objectId={$view.substring(5)} />
+  {:else}
+    <ViewList />
+  {/if}
+</div>
